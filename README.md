00289b03c6a02182c67b6b916fd30a3f301665a3
wmszhe <wmszhe@gmail.com>
2017-08-05 14:32:45 +0800

    完善tinypng

--- README.md
+++ README.md
@@ -5,15 +5,37 @@ Python写的工具类
 ### tinypng
 使用tinypng压缩png图片
 
-> 先安装tinify **pip install --upgrade tinify**
-
 > 每个key每月可压缩500张图片，key在 https://tinypng.com/developers 申请
 
-1.将需要压缩的图片放在uncompressed文件夹下
+```
+先安装tinify和click 
 
-2.执行tinypng.py
+pip3 install --upgrade tinify
 
-3.压缩后的图片会在compressed文件夹下
+pip3 install click
+```
+
+
+- 默认配置 python3 tinypng.py
+
+    1.将需要压缩的图片放在uncompressed文件夹下
+    
+    2.执行tinypng.py
+    
+    3.压缩后的图片会在compressed文件夹下
+   
+- 处理单张图片 python3 tinypng.py -f 图片路径/xx.jpg
+
+    处理后文件名前添加_
+    
+    eg: python3 tinypng.py -f /Users/zhe/Pictures/a.png  生成_a.png
+
+- 处理文件夹中的图片  python3 tinypng.py -d 文件夹路径 
+
+    在文件夹下生成_tinypng文件夹放处理后的文件
+    
+    eg: python3 tinypng.py -d /Users/zhe/Pictures  在Pictures下生成_tinypng文件夹存放处理好的图片
+    
 
 ### qiniu
 上传图片到七牛云存储
--- tinypng/tinypng.py
+++ tinypng/tinypng.py
@@ -3,23 +3,93 @@
 
 import os
 import tinify
+import click
 import Config
 
 tinify.key = Config.pinypng_key
 
-rootDir = os.path.dirname(__file__)
-sourceDir = "/Users/zhe/Documents/_tinypng_file/uncompressed/"
-saveDir = "/Users/zhe/Documents/_tinypng_file/compressed/"
 
-for root, dirs, files in os.walk(sourceDir):
-    size = files.__len__()
-    count = 1
-    for name in files:
-        if (name.startswith(".")):
-            size -= 1
-            continue
-        print("正在处理 %d/%d  %s " % (count, size, name))
-        source = tinify.from_file(sourceDir + name)
-        source.to_file(saveDir + name)
-        count += 1
+# 判断是不是图片
+def isPic(name):
+    # 获取文件名和文件类型
+    filename, filetype = os.path.splitext(name)
+    if (filetype == ".png" or filetype == ".jpg" or filetype == ".jpeg"):
+        return "true"
+    else:
+        print(name + "不是图片")
+
+
+# 压缩图片
+def compressed(inFile, outFile):
+    source = tinify.from_file(inFile)
+    source.to_file(outFile)
+
+
+# 遍历文件夹，压缩图片
+def walkFolder(inFolder, outFilder):
+    for root, dirs, files in os.walk(inFolder):
+        size = files.__len__()
+        count = 1
+        for name in files:
+            if (not isPic(name)):
+                size -= 1
+                continue
+            print("正在处理 %d/%d  %s " % (count, size, name))
+            compressed(inFolder + "/" + name, outFilder + "/" + name)
+            count += 1
+        print("处理完成")
+        break
+
+
+# 没有任何参数，默认对预定文件夹操作
+def default():
+    sourceDir = "/Users/zhe/Documents/_tinypng_file/uncompressed"
+    saveDir = "/Users/zhe/Documents/_tinypng_file/compressed"
+    walkFolder(sourceDir, saveDir)
+
+
+# 压缩单文件
+def compressedFile(inFile):
+    if (not os.path.isfile(inFile)):
+        print("这不是文件")
+        return
+    # 文件路径
+    dirname = os.path.dirname(inFile)
+    # 文件名
+    basename = os.path.basename(inFile)
+    if (not isPic(basename)):
+        return
+    print("开始处理" + basename)
+    compressed(inFile, dirname + "/_" + basename)
     print("处理完成")
+
+
+# 压缩文件夹下所有文件
+def compressedFolder(dir):
+    if (not os.path.isdir(dir)):
+        print("这不是文件夹")
+        return
+    saveDir = dir + "/_tinypng"
+    if (not os.path.isdir(saveDir)):
+        os.mkdir(saveDir)
+    walkFolder(dir, saveDir)
+
+
+@click.command()
+@click.option("-f", "--file", default=None, help="对单文件操作")
+@click.option("-d", "--dir", default=None, help="对文件夹操作")
+def run(file, dir):
+    if (file is not None):
+        compressedFile(file)
+        pass
+    elif (dir is not None):
+        compressedFolder(dir)
+        pass
+    else:
+        default()
+        pass
+
+
+# 程序入口
+if __name__ == "__main__":
+    run()

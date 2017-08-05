9863c6e9a6564f4ec2e7e3f6bc18c54083325699
wmszhe <wmszhe@gmail.com>
2017-08-05 11:30:32 +0800

    七牛上传工具

---
+++ qiniu/qiniu_update.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import os
+from qiniu import Auth, put_file, etag
+
+# ak sk
+access_key = ''
+secret_key = ''
+
+# 构建鉴权对象
+q = Auth(access_key, secret_key)
+
+# 外链前缀
+base_url = ""
+
+# 要上传的空间名
+bucket_name = ""
+
+# 根目录
+rootDir = os.path.dirname(__file__) + "/up_file/"
+
+for root, dirs, files in os.walk(rootDir):
+    size = files.__len__()
+    count = 1
+    for name in files:
+        if (name.startswith(".")):
+            size -= 1
+            continue
+        sourceFile = rootDir + name
+        print("正在上传%s  %d/%d" % (name, count, size))
+        token = q.upload_token(bucket_name, name, 3600)
+        ret, info = put_file(token, name, sourceFile)
+        if (info.status_code == 200):
+            assert ret['key'] == name
+            assert ret['hash'] == etag(sourceFile)
+            print("上传完成，外链url为--  %s" % (base_url + name))

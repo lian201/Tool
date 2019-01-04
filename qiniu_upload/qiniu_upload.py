963e01cecab60bd11c38ddd631bcecfab26fa2c8
wmszhe <wmszhe@gmail.com>
2019-01-04 12:53:32 +0800

    优化

--- qiniu_upload/qiniu_upload.py
+++ qiniu_upload/qiniu_upload.py
@@ -6,7 +6,7 @@ import click
 from qiniu import Auth, put_file, etag
 
 # ak sk
-from qiniu_upload import Config
+import Config
 
 access_key = Config.qiniu_ak
 secret_key = Config.qiniu_sk
@@ -22,42 +22,43 @@ base_url_file = Config.qiniu_base_url_file
 bucket_name_img = Config.qiniu_bucket_name_img
 bucket_name_file = Config.qiniu_bucket_name_file
 
+
 # 上传
-def upload(name, sourceFile):
+def upload(name, source_file):
     filename, filetype = os.path.splitext(name)
-    if (filetype == ".png" or filetype == ".jpg" or filetype == ".jpeg"):
+    if filetype == ".png" or filetype == ".jpg" or filetype == ".jpeg":
         bucket_name = bucket_name_img
         base_url = base_url_img
     else:
         bucket_name = bucket_name_file
         base_url = base_url_file
     token = q.upload_token(bucket_name, name, 3600)
-    ret, info = put_file(token, name, sourceFile)
-    if (info.status_code == 200):
+    ret, info = put_file(token, name, source_file)
+    if info.status_code == 200:
         assert ret['key'] == name
-        assert ret['hash'] == etag(sourceFile)
+        assert ret['hash'] == etag(source_file)
         print("上传完成，外链url为--  %s" % (base_url + name))
     else:
         print("上传失败")
 
 
 # 遍历文件夹
-def walkFolder(rootDir):
-    for root, dirs, files in os.walk(rootDir):
+def walk_folder(root_dir):
+    for root, dirs, files in os.walk(root_dir):
         size = files.__len__()
         count = 1
         for name in files:
-            if (name.startswith(".")):
+            if name.startswith("."):
                 size -= 1
                 continue
-            sourceFile = rootDir + "/" + name
+            source_file = root_dir + "/" + name
             print("正在上传%s  %d/%d" % (name, count, size))
-            upload(name, sourceFile)
+            upload(name, source_file)
 
 
 # 上传单文件
-def uploadFile(file):
-    if (not os.path.isfile(file)):
+def upload_file(file):
+    if not os.path.isfile(file):
         print("这不是文件")
         return
     basename = os.path.basename(file)
@@ -66,28 +67,28 @@ def uploadFile(file):
 
 
 # 上传文件夹里所有文件
-def uploadFolder(dir):
-    if (not os.path.isdir(dir)):
+def upload_folder(dir):
+    if not os.path.isdir(dir):
         print("这不是文件夹")
         return
-    walkFolder(dir)
+    walk_folder(dir)
 
 
 # 没有任何参数，默认对预定文件夹操作
 def default():
-    rootDir = "/Users/zhe/Documents/_qiniu_upfile"
-    walkFolder(rootDir)
+    root_dir = "/Users/zhe/Documents/_qiniu_upfile"
+    walk_folder(root_dir)
 
 
 @click.command()
 @click.option("-f", "--file", default=None, help="上传单文件")
 @click.option("-d", "--dir", default=None, help="上传文件夹里所有文件")
 def run(file, dir):
-    if (file is not None):
-        uploadFile(file)
+    if file is not None:
+        upload_file(file)
         pass
-    elif (dir is not None):
-        uploadFolder(dir)
+    elif dir is not None:
+        upload_folder(dir)
         pass
     else:
         default()
@@ -95,5 +96,5 @@ def run(file, dir):
 
 
 # 程序入口
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     run()
--- tinypng/tinypng.py
+++ tinypng/tinypng.py
@@ -5,38 +5,38 @@ import os
 import tinify
 import click
 
-from tinypng import Config
+import Config
 
-tinify.key = Config.pinypng_key
+tinify.tinify.key = Config.pinypng_key
 
 
 # 判断是不是图片
-def isPic(name):
+def is_pic(name):
     # 获取文件名和文件类型
     filename, filetype = os.path.splitext(name)
-    if (filetype == ".png" or filetype == ".jpg" or filetype == ".jpeg"):
+    if filetype == ".png" or filetype == ".jpg" or filetype == ".jpeg":
         return "true"
     else:
         print(name + "不是图片")
 
 
 # 压缩图片
-def compressed(inFile, outFile):
-    source = tinify.from_file(inFile)
-    source.to_file(outFile)
+def compressed(in_file, out_file):
+    source = tinify.tinify.from_file(in_file)
+    source.to_file(out_file)
 
 
 # 遍历文件夹，压缩图片
-def walkFolder(inFolder, outFilder):
-    for root, dirs, files in os.walk(inFolder):
+def walk_folder(in_folder, out_filder):
+    for root, dirs, files in os.walk(in_folder):
         size = files.__len__()
         count = 1
         for name in files:
-            if (not isPic(name)):
+            if not is_pic(name):
                 size -= 1
                 continue
             print("正在处理 %d/%d  %s " % (count, size, name))
-            compressed(inFolder + "/" + name, outFilder + "/" + name)
+            compressed(in_folder + "/" + name, out_filder + "/" + name)
             count += 1
         print("处理完成")
         break
@@ -44,47 +44,47 @@ def walkFolder(inFolder, outFilder):
 
 # 没有任何参数，默认对预定文件夹操作
 def default():
-    sourceDir = "/Users/zhe/Documents/_tinypng_file/uncompressed"
-    saveDir = "/Users/zhe/Documents/_tinypng_file/compressed"
-    walkFolder(sourceDir, saveDir)
+    source_dir = "/Users/zhe/Documents/_tinypng_file/uncompressed"
+    save_dir = "/Users/zhe/Documents/_tinypng_file/compressed"
+    walk_folder(source_dir, save_dir)
 
 
 # 压缩单文件
-def compressedFile(inFile):
-    if (not os.path.isfile(inFile)):
+def compressed_file(in_file):
+    if not os.path.isfile(in_file):
         print("这不是文件")
         return
     # 文件路径
-    dirname = os.path.dirname(inFile)
+    dirname = os.path.dirname(in_file)
     # 文件名
-    basename = os.path.basename(inFile)
-    if (not isPic(basename)):
+    basename = os.path.basename(in_file)
+    if not is_pic(basename):
         return
     print("开始处理" + basename)
-    compressed(inFile, dirname + "/_" + basename)
+    compressed(in_file, dirname + "/_" + basename)
     print("处理完成")
 
 
 # 压缩文件夹下所有文件
-def compressedFolder(dir):
-    if (not os.path.isdir(dir)):
+def compressed_folder(dir):
+    if not os.path.isdir(dir):
         print("这不是文件夹")
         return
-    saveDir = dir + "/_tinypng"
-    if (not os.path.isdir(saveDir)):
-        os.mkdir(saveDir)
-    walkFolder(dir, saveDir)
+    save_dir = dir + "/_tinypng"
+    if not os.path.isdir(save_dir):
+        os.mkdir(save_dir)
+    walk_folder(dir, save_dir)
 
 
 @click.command()
 @click.option("-f", "--file", default=None, help="处理单文件")
 @click.option("-d", "--dir", default=None, help="处理文件夹里所有文件")
 def run(file, dir):
-    if (file is not None):
-        compressedFile(file)
+    if file is not None:
+        compressed_file(file)
         pass
-    elif (dir is not None):
-        compressedFolder(dir)
+    elif dir is not None:
+        compressed_folder(dir)
         pass
     else:
         default()
